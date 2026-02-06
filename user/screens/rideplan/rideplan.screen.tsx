667462bc1e6b2369992bce922825ad4d4b0c7ecd
asiri-gayashan <gayashanx@gmail.com>
2026-02-06 14:57:51 +0530

    Map locations autofetching part

--- user/screens/rideplan/rideplan.screen.tsx
+++ user/screens/rideplan/rideplan.screen.tsx
@@ -13,10 +13,19 @@ import {
 import style from "./style";
 import { external } from "@/styles/external.style";
 
-import { useState } from "react";
-import { windowHeight } from "@/themes/app.constant";
+import { useCallback, useEffect, useState } from "react";
+import { windowHeight, windowWidth } from "@/themes/app.constant";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
+import { Clock, LeftArrow, PickLocation, PickUpLocation } from "@/utils/icons";
+import { router } from "expo-router";
+import color from "@/themes/app.colors";
+import DownArrow from "@/assets/icons/downArrow";
+import PlaceHolder from "@/assets/icons/placeHolder";
+import { any } from "prop-types";
+import axios from "axios";
+import _ from "lodash";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default function RidePlanScreen() {
   const [places, setPlaces] = useState<any>([]);
@@ -42,6 +51,42 @@ export default function RidePlanScreen() {
 
   const [keyboardAvoidingHeight, setkeyboardAvoidingHeight] = useState(false);
 
+  const fetchPlaces = async (input: any) => {
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/place/autocomplete/json`,
+        {
+          params: {
+            input,
+            key: process.env.EXPO_PUBLIC_GOOGLE_CLOUD_API_KEY,
+            language: "en",
+          },
+        },
+      );
+      setPlaces(response.data.predictions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+   const debouncedFetchPlaces = useCallback(_.debounce(fetchPlaces, 100), []);
+
+  useEffect(() => {
+    if (query.length > 2) {
+      debouncedFetchPlaces(query);
+    } else {
+      setPlaces([]);
+    }
+  }, [query, debouncedFetchPlaces]);
+
+ 
+
+  const handleInputChange = (text: any) => {
+    setQuery(text);
+  };
+
   return (
     <KeyboardAvoidingView
       style={[external.fx_1]}
@@ -55,7 +100,168 @@ export default function RidePlanScreen() {
             style={{ flex: 1 }}
             region={region}
             onRegionChangeComplete={(region) => setRegion(region)}
-          ></MapView>
+          >
+            {marker && <Marker coordinate={marker} />}
+            {currentLocation && <Marker coordinate={currentLocation} />}
+            {currentLocation && marker && (
+              <MapViewDirections
+                origin={currentLocation}
+                destination={marker}
+                apikey={process.env.EXPO_PUBLIC_GOOGLE_CLOUD_API_KEY!}
+                strokeWidth={4}
+                strokeColor="blue"
+              />
+            )}
+          </MapView>
+        </View>
+      </View>
+      <View style={style.contentContainer}>
+        <View style={[style.container]}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <TouchableOpacity onPress={() => router.back()}>
+              <LeftArrow />
+            </TouchableOpacity>
+            <Text
+              style={{
+                margin: "auto",
+                fontSize: windowWidth(25),
+                fontWeight: "600",
+              }}
+            >
+              Plan your ride
+            </Text>
+          </View>
+          {/* picking up time */}
+          <View
+            style={{
+              width: windowWidth(200),
+              height: windowHeight(28),
+              borderRadius: 20,
+              backgroundColor: color.lightGray,
+              alignItems: "center",
+              justifyContent: "center",
+              marginVertical: windowHeight(10),
+            }}
+          >
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Clock />
+              <Text
+                style={{
+                  fontSize: windowHeight(12),
+                  fontWeight: "600",
+                  paddingHorizontal: 8,
+                }}
+              >
+                Pick-up now
+              </Text>
+              <DownArrow />
+            </View>
+          </View>
+
+          {/* picking up location */}
+          <View
+            style={{
+              borderWidth: 2,
+              borderColor: "#000",
+              borderRadius: 15,
+              marginBottom: windowHeight(15),
+              paddingHorizontal: windowWidth(15),
+              paddingVertical: windowHeight(5),
+            }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <PickLocation />
+              <View
+                style={{
+                  width: Dimensions.get("window").width * 1 - 110,
+                  borderBottomWidth: 1,
+                  borderBottomColor: "#999",
+                  marginLeft: 5,
+                  height: windowHeight(20),
+                }}
+              >
+                <Text
+                  style={{
+                    color: "#2371F0",
+                    fontSize: 18,
+                    paddingLeft: 5,
+                  }}
+                >
+                  Current Location
+                </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                paddingVertical: 12,
+              }}
+            >
+              <PlaceHolder />
+
+              <View
+                style={{
+                  marginLeft: 5,
+                  width: Dimensions.get("window").width * 1 - 110,
+                }}
+              >
+                <GooglePlacesAutocomplete
+                  placeholder="Where to?"
+                  onPress={(data, details = null) => {
+                    setkeyboardAvoidingHeight(true);
+                    setPlaces([
+                      {
+                        description: data.description,
+                        place_id: data.place_id,
+                      },
+                    ]);
+                  }}
+                  query={{
+                    key: `${process.env.EXPO_PUBLIC_GOOGLE_CLOUD_API_KEY!}`,
+                    language: "en",
+                  }}
+                  styles={{
+                    textInputContainer: {
+                      width: "100%",
+                    },
+                    textInput: {
+                      height: 38,
+                      color: "#000",
+                      fontSize: 16,
+                    },
+                    predefinedPlacesDescription: {
+                      color: "#000",
+                    },
+                  }}
+                  textInputProps={{
+                    onChangeText: (text: string) => handleInputChange(text),
+                    value: query,
+                    onFocus: () => setkeyboardAvoidingHeight(true),
+                  }}
+                  onFail={(error: any) => console.log(error)}
+                  fetchDetails={true}
+                  debounce={200}
+                />
+              </View>
+            </View>
+          </View>
+
+          {places.map((place: any, index: number) => (
+            <Pressable
+              key={index}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginBottom: windowHeight(20),
+              }}
+              // onPress={() => handlePlaceSelect(place.place_id)}
+            >
+              <PickUpLocation />
+              <Text style={{ paddingLeft: 15, fontSize: 18 }}>
+                {place.description}
+              </Text>
+            </Pressable>
+          ))}
         </View>
       </View>
     </KeyboardAvoidingView>
